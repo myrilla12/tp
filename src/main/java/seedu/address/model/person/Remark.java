60d15bf0567e4a8e72ad2fcc42a98893753a79e8
myrilla12 <myrilla12@gmail.com>
2024-10-01 20:40:21 +0800

    Add remark command

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,35 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+public class Remark {
+    public final String value;
+
+    /**
+     * Constructs an {@code Remark}.
+     *
+     * @param remark A valid remark.
+     */
+    public Remark(String remark) {
+        requireNonNull(remark);
+        value = remark;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this
+                || (other instanceof Remark
+                && value.equals(((Remark) other).value));
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
